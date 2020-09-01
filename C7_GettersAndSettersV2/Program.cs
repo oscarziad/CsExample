a4e815738781eb76c61c1dc93abc06cde721d25c
Oscar Keirouz <oscar.keirouz@learnet.se>
2020-09-01 14:16:37 +0200

    C7

---
+++ C7_GettersAndSettersV2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C7_GettersAndSettersV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            person.AskForDateOfBirth();
+
+            person.AskForName();
+            Console.WriteLine(person.Name);
+        }
+    }
+}
