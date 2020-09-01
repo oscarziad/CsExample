f749e5360dbcd97fa211f7d3296a6ed0b443c38a
Oscar Keirouz <oscar.keirouz@learnet.se>
2020-09-01 11:33:08 +0200

    C3

---
+++ C3_WithAClass/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Schema;
+
+namespace C3_WithAClass
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ C3_WithAClass/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace C3_WithAClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            Console.Write("Welcome Dude! What's your name? ");
+            person.Name = Console.ReadLine();
+
+            Console.Write($"{person.Name} sweet :) How old are you man? ");
+
+            try
+            {
+                person.Age = Convert.ToInt32(Console.ReadLine());
+                //age = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Sorry dude! Your age must be in digits. Try again man.");
+                Console.Write("How old are you man? ");
+
+                try
+                {
+                    person.Age = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Dude are you dumb!");
+                }
+            }
+            finally
+            {
+                Console.WriteLine($"Oh cool man! We are the same age. I am also {person.Age}.");
+            }
+
+            Console.WriteLine($"Dude your name contains {person.Name.Length} charachters.");
+            Console.Write($"{person.Name} where do you live? ");
+            person.City = Console.ReadLine();
+
+
+            Console.WriteLine($"Sweet! My brother lives in {person.City}.");
+        }
+    }
+}
