c878e3e73a7ba90fd6d26ac9e1d909309e7222b3
Oscar Keirouz <oscar.keirouz@learnet.se>
2020-09-03 14:29:22 +0200

    C15

---
+++ C15_Abstract_1/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C15_Abstract_1.Models
+{
+    class Student : Person
+    {
+        public string ClassName { get; set; }
+    }
+}
